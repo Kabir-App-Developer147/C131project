e0ab1d17f2b6190d62668f88d642e06ba3562330
Kabir-App-Developer147 <99479715+Kabir-App-Developer147@users.noreply.github.com>
2022-11-13 16:05:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function Bed()
+{
+    window.location("index1.html");
+}
+function TV()
+{
+    window.location("index2.html");
+}
+function Desk()
+{
+    window.location("index3.html");
+}
+function Bottles()
+{
+    window.location("index4.html");
+}
+function Fruit()
+{
+    window.location("index5.html");
+}
